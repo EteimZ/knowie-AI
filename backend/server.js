d3dfe701283ca7a3ab78e49f59fbee5061297681
mkpouto-inyang <mkay.inyang31@gmail.com>
2024-06-07 08:26:23 +0100

    Reordered directory and converted all html files to ejs

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,39 @@
 const express = require('express');
+const multer  = require('multer');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const llama = require("llamaindex");
+
+require('dotenv').config()
+
+// Configure storage for multer to keep the original filename
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+      cb(null, file.originalname); 
+  }
+});
+const upload = multer({ storage: storage });
+
 const app = express();
 
-// Define a route for the root path ('/')
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..', 'frontend', 'views'));
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'public')));
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('pages/index');
 });
 
-// Define another route for a specific path ('/about')
-app.get('/about', (req, res) => {
-  res.send('This is the about page.');
+app.get('/upload', (req, res) => {
+  res.render('pages/upload');
 });
 
-
-const port = process.env.PORT || 3000;  // Use environment variable for port or default to 3000
+const port = process.env.PORT || 5000;  // Use environment variable for port or default to 3000
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
