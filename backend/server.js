5f136810cce1ee208fd848d81df7d880a3eab5e2
mkpouto-inyang <mkay.inyang31@gmail.com>
2024-06-12 12:53:29 +0100

    Added file viewer feature

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '..', 'frontend', 'views'));
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 
 
